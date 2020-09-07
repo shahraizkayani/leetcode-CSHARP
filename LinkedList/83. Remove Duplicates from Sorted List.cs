6858d0cd44d67ae204224d96b5f9eba3404c1d6f
SHAHRAIZ KAYANI <bcs.12.0002@gmail.com>
2020-09-08 01:03:37 +0500

    Create 83. Remove Duplicates from Sorted List.cs

---
+++ LinkedList/83. Remove Duplicates from Sorted List.cs
@@ -0,0 +1,37 @@
+/*
+
+
+Given a sorted linked list, delete all duplicates such that each element appear only once.
+
+Example 1:
+
+Input: 1->1->2
+Output: 1->2
+
+
+
+*/
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode DeleteDuplicates(ListNode head) {
+        ListNode current = head;
+    while (current != null && current.next != null) {
+        if (current.next.val == current.val) {
+            current.next = current.next.next;
+        } else {
+            current = current.next;
+        }
+    }
+    return head;
+    }
+} 
