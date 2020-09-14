b18c656ddb89131563414aa8087d3d02ddccadbb
SHAHRAIZ KAYANI <bcs.12.0002@gmail.com>
2020-09-14 19:52:28 +0500

    Create 965. Univalued Binary Tree.cs

---
+++ 965. Univalued Binary Tree.cs
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    public bool IsUnivalTree(TreeNode root) {
+        
+        if(root ==null){
+            return true;
+        }
+      if(root.left != null)
+        if(root.val != root.left.val)
+            return false;
+    
+    if(root.right != null)
+        if(root.val != root.right.val)
+            return false;
+        
+     return    IsUnivalTree(root.left)
+     &&   IsUnivalTree(root.right);
+    }
+}
